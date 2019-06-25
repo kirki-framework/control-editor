aa8c0db52be1a2aefaff95080c32968fec380ac5
Danny Cooper <info@dannycooper.org>
2019-06-25 18:02:03 +0100

    add media_buttons argument

--- src/assets/scripts/control.js
+++ src/assets/scripts/control.js
@@ -1,18 +1,17 @@
 /* global tinyMCE */
-wp.customize.controlConstructor['kirki-editor'] = wp.customize.kirkiDynamicControl.extend( {
+wp.customize.controlConstructor[ 'kirki-editor' ] = wp.customize.kirkiDynamicControl.extend( {
 
 	initKirkiControl: function( control ) {
 		var element, editor, id;
 		control = control || this;
 		element = control.container.find( 'textarea' );
 		id      = 'kirki-editor-' + control.id.replace( '[', '' ).replace( ']', '' );
-
 		wp.editor.initialize( id, {
 			tinymce: {
 				wpautop: true
 			},
 			quicktags: true,
-			mediaButtons: true
+			mediaButtons: control.params.mediaButtons
 		} );
 
 		editor = tinyMCE.get( id );
